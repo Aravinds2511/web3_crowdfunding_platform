23be3a5c7b72163f72b1c1cf7876efce4b8150f4
arvndblocker"nn <aravindvasang@gmail.com>
2023-10-10 10:42:24 +0530

    crowd_funding_dapp

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,55 @@
 import React from "react";
 
 const Footer = () => {
-    return <div>Footer</div>
-}
+    const productList = ["Market", "ERC20 Token", "Donation"];
+    const contactList = ["support@cryptoking.com", "info@example.com", "Contact us"];
+    const usefulLink = ["Home", "About us", "Company Bio"];
 
+    return (
+        <footer className="text-center text-white backgroundMain lg:text-left">
+            <div className="mx-6 py-10 text-center md:text-left">
+                <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
+                    <div className="">
+                        <h6 className="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start">
+                            CrwFunds
+                        </h6>
+                        <p>
+                            Here you get funding for your projects and can fund projects you are excited about...
+                        </p>
+                    </div>
+                    <div className="">
+                        <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+                            Products
+                        </h6>
+                        {productList.map((el, i) => (
+                            <p className="mb-4" key={i + 1}>
+                                <a href="#!">{el}</a>
+                            </p>
+                        ))}
+                    </div>
+                    <div className="">
+                        <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+                            Useful Links
+                        </h6>
+                        {usefulLink.map((el, i) => (
+                            <p className="mb-4" key={i + 1}>
+                                <a href="#!">{el}</a>
+                            </p>
+                        ))}
+                    </div>
+                    <div className="">
+                        <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+                            Contact
+                        </h6>
+                        {contactList.map((el, i) => (
+                            <p className="mb-4" key={i + 1}>
+                                <a href="#!">{el}</a>
+                            </p>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
 export default Footer;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,38 +11,38 @@ const Navbar = () => {
     const menuList = ["White Paper", "Project", "Donation", "Members"];
 
     return(
-        <div className="backgroundMain">
-            <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-                <div className="relative flex items-center justify-between">
-                    <div className="flex items-center">
+        <div class="backgroundMain">
+            <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+                <div class="relative flex items-center justify-between">
+                    <div class="flex items-center">
                         <a
                           href="/"
                           aria-label="Company"
                           title="Company"
-                          className="inline-flex items-center mr-8">
+                          class="inline-flex items-center mr-8">
                             <Logo color="text-white" />
-                            <span className="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
+                            <span class="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
                                 Crwfunds
                             </span>
                           </a>
                         
-                          <ul className="flex items-center hidden space-x-8 lg:flex">
-                            {menuList.map((el, i) => {
-                                <li key={i + 1}>
-                                    <a
-                                      href="/"
-                                      aria-label="Our product"
-                                      title="Our product"
-                                      className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400">
+                          <ul class="flex items-center hidden space-x-8 lg:flex">
+                          {menuList.map((el, i) => (
+                              <li key={i + 1}>
+                                  <a 
+                                    href="/"
+                                    aria-label="Our product"
+                                    title="Our product"
+                                    class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-deep-purple-accent-400">
                                         {el}
-                                      </a>
-                                </li>
-                            })}
+                                  </a>
+                              </li>
+                            ))}
                           </ul>
                         
                     </div>
                     {!currentAccount && (
-                        <ul className="flex items-center hidden space-x-8 lg:flex">
+                        <ul class="flex items-center hidden space-x-8 lg:flex">
                             <li>
                             <button  onClick={() => connectWallet()} class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 transform hover:scale-105 hover:shadow-lg"
                                       aria-label="Sign up"
@@ -54,7 +54,7 @@ const Navbar = () => {
                         </ul>
                     )}
 
-                    <div className="lg:hidden z-40">
+                    <div class="lg:hidden z-40">
                         <button 
                           aria-label="Open Menu"
                           title="Open Menu"
@@ -63,18 +63,18 @@ const Navbar = () => {
                             <Menu />
                           </button>
                           {isMenuOpen && (
-                            <div className="absolute top-0 left-0 w-full">
-                                <div className="p-5 bg-white border rounded shadow-sm">
-                                    <div className="flex items-center justify-between mb-4">
+                            <div class="absolute top-0 left-0 w-full">
+                                <div class="p-5 bg-white border rounded shadow-sm">
+                                    <div class="flex items-center justify-between mb-4">
                                         <div>
                                             <a
                                               href="/"
                                               aria-label="Company"
                                               title="Company"
-                                              className="inline-flex items-center">
+                                              class="inline-flex items-center">
                                                 <Logo color="text-black" />
-                                                <span className="ml-2 text-xl font-bold tracking-wide textgray-800 uppercase">
-                                                    Company
+                                                <span class="ml-2 text-xl font-bold tracking-wide textgray-800 uppercase">
+                                                    Crwfunds
                                                 </span>
                                               </a>
                                         </div>
@@ -82,21 +82,21 @@ const Navbar = () => {
                                             <button
                                               aria-label="Close Menu"
                                               title="Close Menu"
-                                              className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                              class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                                               onClick={() => setIsMenuOpen(false)}>
                                                 <Close />
                                               </button>
                                         </div>
                                     </div>
                                     <nav>
-                                        <ul className="space-y-4">
+                                        <ul class="space-y-4">
                                             {menuList.map((el, i) => (
                                                 <li key={i + 1}>
                                                     <a 
                                                       href="/"
                                                       aria-label="Our product"
                                                       title="Our product"
-                                                      className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
+                                                      class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
                                                         {el}
                                                       </a>
                                                 </li>
@@ -104,7 +104,7 @@ const Navbar = () => {
                                             <li>
                                                 <a
                                                  href="/"
-                                                 className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 transform hover:scale-105 hover:shadow-lg"
+                                                 class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 transform hover:scale-105 hover:shadow-lg"
                                                  aria-label="Sign up"
                                                  title="Sign up">
                                                     Connect Wallet
